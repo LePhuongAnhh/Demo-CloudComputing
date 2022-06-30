a71d0aabfc5051d0973e7ed34e3abae82993407f
LePhuongAnhh <95765597+LePhuongAnhh@users.noreply.github.com>
2022-07-01 01:25:07 +0700

    Update
    update code

--- code/main.js
+++ code/main.js
@@ -11,17 +11,10 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb+srv://phuonganh:phuonganh244@cluster0.55amjub.mongodb.net/test'
 
 
-
-
-
-
-//**********index****************/
+//***88888*******index****************/
 app.get('/index', async (req, res)=>{
-    //1. kết nối đến server có địa chỉ trong url
     let server = await MongoClient.connect(url)
-    //truy cập Database ATN
     let dbo = server.db("ATNToys")
-    //get data
     let products = await dbo.collection('product').find().toArray()
     res.render('homepage', {'products':products})
 })
@@ -29,67 +22,71 @@ app.get('/index', async (req, res)=>{
 
 //**************search***********//
 app.post('/search', async(req, res)=>{
-    let name = req.body.txtName 
-    //1. kết nối đến server có địa chỉ trong url
+    let name = req.body.txtSame 
     let server = await MongoClient.connect(url)
-    //truy cập Database ATN
     let dbo = server.db("ATNToys")
-    //get data
     let products = await dbo.collection('product').find({'name':new RegExp(name, 'i')}).toArray()
     res.render('homepage', {'products':products})
 })
 
 
 //**************Create***********//
+app.get('/insert', (req, res)=>{
+    res.render("newProduct")
+})
 app.post('/newProduct', async (req, res)=>{
     let name = req.body.txtName
     let price = req.body.txtPrice
     let picture = req.body.txtPicture
     let description = req.body.txtDescription
     
-    if(name.length <= 5){
-        res.render('newProduct',{'nameError': 'Name not less than five characters'})
+    if(name.length < 1){
+        res.render('newProduct',{'nameError': 'Cannot be left blank'})
         return
     }
+
+
     let product = {
         'name':name,
         'price':price,
         'picture':picture,
         'description':description
     }
-    //1. kết nối đến server có địa chỉ trong url
+
     let server = await MongoClient.connect(url)
-    //truy cập Database ATN
     let dbo = server.db("ATNToys")
-    //insert product vào database
-    await dbo.collection("product").insert(product)
-    //quay lại trang home
-    res.redirect('/homepage')
+    await dbo.collection("product").insertOne(product)
+    res.redirect('/index')   
 })
-app.get('/insert', (req, res)=>{
-    res.render("newProduct")
-})
+// //eps kieeur thanhf objiect id
+//laays ra id đặt vào đối tượng req trong thuộc tính params. trungf vs id treen url
+
+
 
 //***************Update*************/
 app.get('/update/:_id', async (req, res) => {
     var id = req.params._id;
     var good_id = new ObjectId(id);
+
     let server = await MongoClient.connect(url) 
     let dbo = server.db("ATNToys")
     let products = await dbo.collection('product').find({ '_id': good_id}).limit(1).toArray()
     res.render('update', {'products': products[0]})
 })
+
 app.post('/edit/:_id', async (req, res) => {
     let name = req.body.txtName
     let price = req.body.txtPrice
     let picture = req.body.txtPicture
     let description = req.body.txtDescription
+
     var id = req.params._id;
     var good_id = new ObjectId(id);
 
-    let server = await MongoClient.connect(url) 
+    let server = await MongoClient.connect(url)
     let dbo = server.db("ATNToys")
-    await dbo.collection('product').updateOne({ '_id': good_id }, {$set:{'_id': good_id,'name': name, 'price': price, 'picture': picture, 'description': description}})
+    await dbo.collection("product").updateOne(product),
+    {$set:{'_id': good_id,'name': name, 'price': price, 'picture': picture, 'description': description}}
     res.redirect('/index')
 })
 
@@ -99,29 +96,13 @@ app.get('/delete/:_id', async (req, res) => {
     var id = req.params._id;
     var id2 = new ObjectId(id);
 
-    //1. kết nối đến server có địa chỉ trong url
-    let server = await MongoClient.connect(url) // await là đợi công việc này hoàn thành mới làm công việc tiếp theo. 
-    //phải có async mới dùng được await 
-    //2. truy cập database ATNToys
+    let server = await MongoClient.connect(url) 
     let dbo = server.db("ATNToys")
     await dbo.collection('product').deleteOne({'_id': id2})
     res.redirect('/index')
 })
-// app.get('/delete/:_id', async (req, res)=>{
-//     //transform your param into an ObjectId
-//     var id = req.params._id;
-//     var id2 = new ObjectId(id);
-
-//     //1. kết nối đến server có địa chỉ trong url
-//     let server = await MongoClient.connect(url) // await là đợi công việc này hoàn thành mới làm công việc tiếp theo. 
-//     //phải có async mới dùng được await 
-//     //2. truy cập database ATNToys
-//     let dbo = server.db("ATNToys")
-//     let products = await dbo.collection('product').find({ '_id': id2}).limit(1).toArray()
-//     res.render('delete', {'products': products[0]})
-// })
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 app.listen(PORT)
 console.log('Running..................')
